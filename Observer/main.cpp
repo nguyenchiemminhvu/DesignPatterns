7c81989a65cb05bbddc75cc46eba6711b70207ac
vu3.nguyen <vu3.nguyen@lge.com>
2023-10-24 16:00:15 +0900

    update

--- Observer/main.cpp
+++ Observer/main.cpp
@@ -13,139 +13,139 @@ class BrokerService;
 
 enum EVENT_ID
 {
-	SYSTEM_STATUS_CHANGED,
+    SYSTEM_STATUS_CHANGED,
 
-	EVENT_ID_TOTAL
+    EVENT_ID_TOTAL
 };
 
 class Client
 {
 public:
-	Client() {}
-	virtual ~Client() {}
+    Client() {}
+    virtual ~Client() {}
 
-	void OnEventNotified(EVENT_ID id, void* arg)
-	{
-		switch (id)
-		{
-			case EVENT_ID::SYSTEM_STATUS_CHANGED:
-			{
-				int status = *(int*)arg;
-				printf("Client received system status: %d\n", status);
-				break;
-			}
-			default:
-			{
-				break;
-			}
-		}
-	}
+    void OnEventNotified(EVENT_ID id, void* arg)
+    {
+        switch (id)
+        {
+            case EVENT_ID::SYSTEM_STATUS_CHANGED:
+            {
+                int status = *(int*)arg;
+                printf("Client received system status: %d\n", status);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+    }
 
 private:
-	friend class BrokerService;
+    friend class BrokerService;
 };
 
 class BrokerService
 {
 public:
-	static BrokerService* GetInstance();
-	~BrokerService() {}
+    static BrokerService* GetInstance();
+    ~BrokerService() {}
 
-	void RegisterEvent(Client* cli, Service* serv, EVENT_ID id)
-	{
-		manager[serv][id].push_back(cli);
-	}
+    void RegisterEvent(Client* cli, Service* serv, EVENT_ID id)
+    {
+        manager[serv][id].push_back(cli);
+    }
 
-	void NotifyClient(Service* serv, EVENT_ID id, void* arg)
-	{
-		if (manager.find(serv) == manager.end())
-		{
-			return;
-		}
+    void NotifyClient(Service* serv, EVENT_ID id, void* arg)
+    {
+        if (manager.find(serv) == manager.end())
+        {
+            return;
+        }
 
-		std::vector<Client*>& clients = manager[serv][id];
-		for (Client* client : clients)
-		{
-			client->OnEventNotified(id, arg);
-		}
-	}
+        std::vector<Client*>& clients = manager[serv][id];
+        for (Client* client : clients)
+        {
+            client->OnEventNotified(id, arg);
+        }
+    }
 
 private:
-	static BrokerService* instance;
-	BrokerService() {}
+    static BrokerService* instance;
+    BrokerService() {}
 
-	std::unordered_map<Service*, std::unordered_map<EVENT_ID, std::vector<Client*>>> manager;
+    std::unordered_map<Service*, std::unordered_map<EVENT_ID, std::vector<Client*>>> manager;
 };
 BrokerService* BrokerService::instance = nullptr;
 BrokerService* BrokerService::GetInstance()
 {
-	if (instance == nullptr)
-	{
-		instance = new BrokerService();
-	}
+    if (instance == nullptr)
+    {
+        instance = new BrokerService();
+    }
 
-	return instance;
+    return instance;
 }
 
 class Service
 {
 public:
-	Service() {}
-	virtual ~Service() {}
+    Service() {}
+    virtual ~Service() {}
 
-	void NotifyClient(EVENT_ID id, void* arg)
-	{
-		BrokerService::GetInstance()->NotifyClient(this, id, arg);
-	}
+    void NotifyClient(EVENT_ID id, void* arg)
+    {
+        BrokerService::GetInstance()->NotifyClient(this, id, arg);
+    }
 
 private:
-	friend class BrokerService;
+    friend class BrokerService;
 };
 
 class System : public Service
 {
 public:
-	System()
-		: Service(), status(0)
-	{
-		printf("System init\n");
+    System()
+        : Service(), status(0)
+    {
+        printf("System init\n");
 
-		pthread_attr_t attr;
-		pthread_attr_init(&attr);
-		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
-		pthread_t t; 
-		pthread_create(&t, &attr, &System::run, this);
-	}
-	virtual ~System() {}
+        pthread_t t; 
+        pthread_create(&t, &attr, &System::run, this);
+    }
+    virtual ~System() {}
 
 private:
-	static void* run(void* arg)
-	{
-		printf("%x: %s\n", pthread_self(), __func__);
-		System *pSys = (System*)arg;
+    static void* run(void* arg)
+    {
+        printf("%x: %s\n", pthread_self(), __func__);
+        System *pSys = (System*)arg;
 
-		while (true)
-		{
-			pSys->status++;
-			pSys->status %= 2;
+        while (true)
+        {
+            pSys->status++;
+            pSys->status %= 2;
 
-			pSys->NotifyClient(EVENT_ID::SYSTEM_STATUS_CHANGED, (void*)&pSys->status);
+            pSys->NotifyClient(EVENT_ID::SYSTEM_STATUS_CHANGED, (void*)&pSys->status);
 
-			sleep(1);
-		}
+            sleep(1);
+        }
 
-		return (void*)0;
-	}
+        return (void*)0;
+    }
 private:
-	int status; // 0: SUSPENDING 1: RUNNING
+    int status; // 0: SUSPENDING 1: RUNNING
 };
 
 class Navi : public Client
 {
 public:
-	Navi() {}
-	virtual ~Navi() {}
+    Navi() {}
+    virtual ~Navi() {}
 
 private:
 
@@ -153,14 +153,14 @@ private:
 
 int main()
 {
-	std::shared_ptr<System> sys = std::make_shared<System>();
-	Navi navi_app;
-	BrokerService::GetInstance()->RegisterEvent(&navi_app, sys.get(), EVENT_ID::SYSTEM_STATUS_CHANGED);
+    std::shared_ptr<System> sys = std::make_shared<System>();
+    Navi navi_app;
+    BrokerService::GetInstance()->RegisterEvent(&navi_app, sys.get(), EVENT_ID::SYSTEM_STATUS_CHANGED);
 
-	while (true)
-	{
+    while (true)
+    {
 
-	}
+    }
 
-	return 0;
+    return 0;
 }
