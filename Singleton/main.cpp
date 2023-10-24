abd366191db93e11e7478432e0db3e221d4f49b9
vu3.nguyen <vu3.nguyen@lge.com>
2023-10-24 16:01:28 +0900

    singleton

--- Singleton/main.cpp
+++ Singleton/main.cpp
@@ -6,9 +6,35 @@
 #include <memory>
 #include <queue>
 
+template <typename T>
+class Singleton {
+public:
+    static T& getInstance() {
+        static T instance;
+        return instance;
+    }
+
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+protected:
+    Singleton() = default;
+    ~Singleton() = default;
+};
+ 
+class MySingleton : public Singleton<MySingleton> {
+public:
+    void doSomething() {
+        // Implement your singleton logic here
+    }
+
+    // Add any other member functions or variables as needed
+};
+
 int main()
 {
+    MySingleton& instance = MySingleton::getInstance();
+    instance.doSomething();
 
-
-	return 0;
+    return 0;
 }
